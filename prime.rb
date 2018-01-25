8399047786287de4597303b514f8b66662028747
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-25 15:21:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,18 @@
 def prime?(n)
   if n <= 1
-    false
+    return false
   elsif n <= 3
-    true
+    return true
   elsif n%2==0 || n%3==0
-    false
+    return false
   else
     i = 5
     while i*i <= n
       if n%i==0 || n%(i+2)==0
-        false
+        return false
       end
       i += 6
     end
+    return true
   end
 end
