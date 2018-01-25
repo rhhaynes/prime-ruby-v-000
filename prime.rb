e6059e67d440d7e6cfa81490ca05f4de746dc802
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-25 15:23:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -8,9 +8,7 @@ def prime?(n)
   else
     i = 5
     while i*i <= n
-      if n%i==0 || n%(i+2)==0
-        return false
-      end
+      return false if n%i==0 || n%(i+2)==0
       i += 6
     end
     return true
